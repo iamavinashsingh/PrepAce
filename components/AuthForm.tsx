514b8c52dcf4ab113f17613495ee2facfd7d4261
Avinash Singh <imavinashsingh2@gmail.com>
2025-06-29 16:32:05 +0530

    Upadted Authentication via firebase

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,115 +1,158 @@
-"use client"
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod"
-import { Button } from "@/components/ui/button"
-import {Form} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import Image from "next/image";
+"use client";
+
+import { z } from "zod";
 import Link from "next/link";
+import Image from "next/image";
 import { toast } from "sonner";
-import FormField from "./FormField";
+import { auth } from "@/firebase/client";
+import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-    // ========================         AUTH FORM SCHEMA   =============================
+import {  createUserWithEmailAndPassword,  signInWithEmailAndPassword,} from "firebase/auth";
 
-    const authFormSchema = (type:FormType)=>{
-        return z.object({
-            name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
-            email: z.string().email(),
-            password: z.string().min(8),
-        })
-    }
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
 
-    // =========================           DEFINING FORM             ===========================
+import { signIn, signUp } from "@/lib/actions/auth.action";
+import FormField from "./FormField";
 
-    const AuthForm = ({type}:{type: FormType}) => {
-        const router = useRouter();
+const authFormSchema = (type: FormType) => {
+    return z.object({
+        name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+        email: z.string().email(),
+        password: z.string().min(3),
+    });
+    };
 
-        const formSchema = authFormSchema(type)
-        const form = useForm<z.infer<typeof formSchema>>({
-            resolver: zodResolver(formSchema),
-            defaultValues: {
-                name: "",
-                email: "",
-                password: "",
-            },
-    })
+    const AuthForm = ({ type }: { type: FormType }) => {
+    const router = useRouter();
 
-    
+    const formSchema = authFormSchema(type);
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+        name: "",
+        email: "",
+        password: "",
+        },
+    });
 
-    //  =======================        FORM SUBMIT HANDLER            ===================================
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
+        try {
+        if (type === "sign-up") {
+            const { name, email, password } = data;
 
-        try{
-            if(type ==='sign-up'){
-                toast.success('Account created successfully. Please sign in.');
-                router.push('/sign-in')
+            const userCredential = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+            );
+
+            const result = await signUp({
+            uid: userCredential.user.uid,
+            name: name!,
+            email,
+            password,
+            });
+
+            if (!result.success) {
+            toast.error(result.message);
+            return;
             }
-            else{
-                toast.success('Signed in  successfully. ');
-                router.push('/')
+
+            toast.success("Account created successfully. Please sign in.");
+            router.push("/sign-in");
+        } else {
+            const { email, password } = data;
+
+            const userCredential = await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+            );
+
+            const idToken = await userCredential.user.getIdToken();
+            if (!idToken) {
+            toast.error("Sign in Failed. Please try again.");
+            return;
             }
 
-        }
-        catch(error){
-            console.log("Error is ", (error as Error).message);
-            toast.error(`There was an error ${error}`);
-        }
-    
-    }
+            await signIn({
+            email,
+            idToken,
+            });
 
-    const isSignIn = type ==='sign-in';
+            toast.success("Signed in successfully.");
+            router.push("/");
+        }
+        } catch (error) {
+        console.log(error);
+        toast.error(`There was an error: ${error}`);
+        }
+    };
+
+    const isSignIn = type === "sign-in";
 
     return (
         <div className="card-border lg:min-w-[566px]">
-            <div className="flex flex-col gap-6 card py-14 px-10">
-                <div className="flex flex-row gap-2 justify-center">
-                    <Image src="/logo.svg" alt="PrepAce's Logo" height={32} width={38}/>
-                    <h2 className="text-primary-100">PrepAce</h2>
-                </div>
-                <h3>Practice Job interviews with AI</h3>    
-                {/* =======================         FORM              =============================== */}
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} 
-                        className="w-full space-y-6 mt-4 form">
-                        {/*------------------------               NAME            ---------------------------- */}
-                        {!isSignIn && (
-                            <FormField 
-                                control={form.control} 
-                                name="name" 
-                                lable="Name" 
-                                placeholder="Your Name" 
-                            />
-                        )}
-                        {/*------------------------               Email            ---------------------------- */}
-                        <FormField 
-                                control={form.control} 
-                                name="email" 
-                                lable="Email" 
-                                placeholder="example@email.com"
-                                type="email" 
-                            />
-                        {/*------------------------               PASSWORD            ---------------------------- */}
-                        <FormField 
-                                control={form.control} 
-                                name="password" 
-                                lable="Password" 
-                                placeholder="Your Password" 
-                                type="password"
-                            />
-                        <Button className="btn" type="submit">{isSignIn ? 'Sign in':'Create an Account'}</Button>
-                    </form>
-                </Form>
-                <p className="text-center">
-                    {isSignIn ? 'No account yet ? ':'Have an account already ? '}
-                    <Link href={!isSignIn ? '/sign-in':'sign-up'} className='font-bold text-user-primary ml-1 '>
-                    {!isSignIn ? 'Sign In':'Sign Up'}
-                    </Link>
-                </p>
+        <div className="flex flex-col gap-6 card py-14 px-10">
+            <div className="flex flex-row gap-2 justify-center">
+            <Image src="/logo.svg" alt="logo" height={32} width={38} />
+            <h2 className="text-primary-100">PrepWise</h2>
             </div>
-        </div>
-    )
-}
 
-export default AuthForm
+            <h3>Practice job interviews with AI</h3>
+
+            <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="w-full space-y-6 mt-4 form"
+            >
+                {!isSignIn && (
+                <FormField
+                    control={form.control}
+                    name="name"
+                    label="Name"
+                    placeholder="Your Name"
+                    type="text"
+                />
+                )}
+
+                <FormField
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Your email address"
+                type="email"
+                />
+
+                <FormField
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+                type="password"
+                />
+
+                <Button className="btn" type="submit">
+                {isSignIn ? "Sign In" : "Create an Account"}
+                </Button>
+            </form>
+            </Form>
+
+            <p className="text-center">
+            {isSignIn ? "No account yet?" : "Have an account already?"}
+            <Link
+                href={!isSignIn ? "/sign-in" : "/sign-up"}
+                className="font-bold text-user-primary ml-1"
+            >
+                {!isSignIn ? "Sign In" : "Sign Up"}
+            </Link>
+            </p>
+        </div>
+        </div>
+    );
+};
+
+export default AuthForm;
--- firebase/admin.ts
+++ firebase/admin.ts
@@ -2,8 +2,13 @@ import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
-// Initialize Firebase Admin SDK
 function initFirebaseAdmin() {
+    if (!process.env.FIREBASE_PROJECT_ID || 
+        !process.env.FIREBASE_CLIENT_EMAIL || 
+        !process.env.FIREBASE_PRIVATE_KEY) {
+        throw new Error("Missing Firebase Admin environment variables 😔");
+    }
+
     const apps = getApps();
 
     if (!apps.length) {
@@ -11,8 +16,7 @@ function initFirebaseAdmin() {
         credential: cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            // Replace newlines in the private key
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
         }),
         });
     }
