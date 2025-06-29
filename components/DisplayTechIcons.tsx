1e0349a9840b551e880f0d194cb2f3e912b703b7
Avinash Singh <imavinashsingh2@gmail.com>
2025-06-29 15:44:21 +0530

    Updated firebase setup and added new files

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -6,7 +6,7 @@ const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
     const techIcons = await getTechLogos(techStack);
 
     return (
-        <div className="flex flex-row gap-4">
+        <div className="flex flex-row ">
         {techIcons.slice(0, 3).map(({ tech, url }, index) => (
             <div
             key={tech}
